f0241c3f596a5e06dc8b0672043b88d549a55636
Itamar Borges da Silva Filho <itamarborges@gmail.com>
2018-03-31 00:09:43 -0300

    Adding Android Library

--- jokelib/src/main/java/com/example/jokelib/JokeFactory.java
+++ jokelib/src/main/java/com/example/jokelib/JokeFactory.java
@@ -1,4 +1,20 @@
 package com.example.jokelib;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class JokeFactory {
+
+    static String[] jokes = {"I lost my job at the bank on my very first day. – A woman asked me to check her balance, so I pushed her over.",
+                      "Today a man knocked on my door and asked for a small donation towards the local swimming pool. – I gave him a glass of water.",
+                      "An old teacher asked her student, “If I say, ‘I am beautiful,’ which tense is that?” The student replied, “It is obviously past.”",
+                      "My doctor called me fat. I told him I wanted a second opinion and he said, “OK, you’re ugly too.”",
+                      "Why did the picture go to jail? Cause it was framed!"};
+
+    public static String getAJoke() {
+        Random rand = new Random();
+        int x = rand.nextInt(5);
+        return jokes[x];
+
+    }
 }
